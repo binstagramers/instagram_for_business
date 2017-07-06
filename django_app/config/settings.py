faa8006c3880e7bb0da8f133a5114895c63c35b5
GAIUS8 <gaius827@gmail.com>
2017-07-06 17:27:59 +0900

    member model 생성 및 폴더 정리

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+# Custom 유저 정의
+AUTH_USER_MODEL = 'member.User'
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
