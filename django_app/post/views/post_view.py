758a809262c711af2f257008e47a4cb1a3cfdf86
GAIUS8 <gaius827@gmail.com>
2017-07-07 00:20:20 +0900

    중간단계 적용

--- django_app/post/views/post_view.py
+++ django_app/post/views/post_view.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from ..form import PostForm
 from ..models import Post
 
 User = get_user_model()
@@ -13,3 +14,11 @@ def post_list(request):
         'posts': posts
     }
     return render(request, 'post/post_list.html', context)
+
+
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save(author=request.user)
+        return redirect('post:post_list')
